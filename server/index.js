45ce3dc65ac738b9d0ff334df74c8d7bc7aa6be6
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2023-07-10 22:16:41 +0530

    Updated message Routes and Controller

--- server/index.js
+++ server/index.js
@@ -1,23 +1,26 @@
-const express =require ("express");
-const cors =require("cors");
-const mongoose =require("mongoose");
-
-
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
 const app = express();
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL,{
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(()=>{
+}).then(() => {
     console.log("Connected to MongoDB");
-}).catch((err)=>{
+}).catch((err) => {
     console.log("error");
 });
 
-const server=app.listen(process.env.PORT,()=>{
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server Started on Port ${process.env.PORT}`);
 });
